d5dd87a693d9385238172efe9b3b31a6c582a34e
andrewbubar <andrew.bubar@yahoo.com>
2016-08-31 15:45:20 -0700

    Update updateUser.py

--- examples/python/updateUser.py
+++ examples/python/updateUser.py
@@ -67,20 +67,20 @@ while (1):
 			sel = raw_input("What would you like to update for this user?")
 			if sel == 'Name':
 			 name = raw_input("What is the new name?")
-		  	params = name, rfidNumber
+		  	 params = name, rfidNumber
 			 cur.execute("UPDATE PERMISSIONS SET Name = ? WHERE ID = ?", params)
 			if sel == 'Laser':
 			  laser = raw_input("Y or N for Laser Cutter?")
 			  params = laser, rfidNumber
 			  cur.execute("UPDATE PERMISSIONS SET Laser = ? WHERE ID = ?", params)
 			if sel == 'Printer':
-		  	printer = raw_input("Y or N for 3-D Printer?")
+		  	 printer = raw_input("Y or N for 3-D Printer?")
 			 params = printer, rfidNumber
 			 cur.execute("UPDATE PERMISSIONS SET Printer = ? WHERE ID = ?", params)
 			if sel == 'Solder':
 			 solder = raw_input("Y or N for Soldering Machine?")
-			  params = solder, rfidNumber
-			  cur.execute("UPDATE PERMISSIONS SET Solder = ? WHERE ID = ?", params)
+			 params = solder, rfidNumber
+			 cur.execute("UPDATE PERMISSIONS SET Solder = ? WHERE ID = ?", params)
 			con.commit()
 			con.close()
 			blueLED.off()
