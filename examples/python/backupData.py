8a2aa0e873d977d44e6dcbe4cfb1890912348ad6
andrewbubar <andrew.bubar@yahoo.com>
2016-09-23 14:59:21 -0700

    Update backupData.py

--- examples/python/backupData.py
+++ examples/python/backupData.py
@@ -1,1 +1,8 @@
+import sqlite3 as lite
 
+with open('backup.csv', 'w+') as write_file
+  con = lite.connect('makerspace.db')
+  cur = con.cursor()
+  for row in cur.execute("SELECT * FROM USED"):
+    write_file.write(','.join(row))
+    write_file.write('\n')
