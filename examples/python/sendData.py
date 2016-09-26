35500cae229b27ed86a5029a1aa7a05fad6e54e9
andrewbubar <andrew.bubar@yahoo.com>
2016-09-26 16:00:30 -0700

    Create sendData.py

---
+++ examples/python/sendData.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python
+
+import sqlite3 as lite
+import csv
+
+with open('backup.csv', 'w+') as write_file:
+	con = lite.connect('makerspace.db')
+	cur = con.cursor()
+	for row in cur.execute('SELECT * FROM USED'):
+		write_file.write(','.join(row))
+		write_file.write('\n')
+    
