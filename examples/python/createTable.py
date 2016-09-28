4e4e19a9d20a2b8e1cfef5bbcabffb80df7b18a3
andrewbubar <andrew.bubar@yahoo.com>
2016-09-28 08:40:53 -0700

    Update createTable.py

--- examples/python/createTable.py
+++ examples/python/createTable.py
@@ -1,19 +1,26 @@
 #!/usr/bin/python
 import sqlite3 as lite
 
-con = lite.connect('makerspace.db')
+con = lite.connect('makerspace.db') # connect to makerspace database
 
 people = (
 	("9621023222", 'Andrew Bubar', 'Y', 'Y', 'Y'),
         ("321143517", 'Hunter Pickett', 'N', 'N', 'N'),
         )
+test = (("1234567890", 'Test', 'Test', 'Test'))
 
 with con:
-	cur = con.cursor()
+	cur = con.cursor() # set cursor
 	
+	# Drop table Permissions, recreate it, add values to it
 	cur.execute("DROP TABLE IF EXISTS PERMISSIONS")
 	cur.execute("CREATE TABLE PERMISSIONS(ID TEXT, Name TEXT, Laser TEXT, Printer TEXT, Solder TEXT)")
     	cur.executemany("INSERT INTO Permissions VALUES(?,?,?,?,?)",people)
+	
+	# Drop table Used, recreate it, add values to it
+	cur.execute("DROP TABLE IF EXISTS USED")
+	cur.execute("CREATE TABLE USED(ID TEXT, Name TEXT, TimeStart BLOB, TimeStop BLOB)")
+	cur.execute("INSERT INTO USED VALUES(?,?,?,?)", test)
 
-con.commit()
-con.close()
+con.commit() # commit values to makerspace database
+con.close() # close makerspace database
