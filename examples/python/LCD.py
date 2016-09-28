d50605611f72f4b4a59f38294943e87eab304b67
andrewbubar <andrew.bubar@yahoo.com>
2016-09-28 08:44:33 -0700

    Update LCD.py

--- examples/python/LCD.py
+++ examples/python/LCD.py
@@ -1,13 +1,15 @@
+#!/usr/bin/python
+
 import mraa, time
 import pyupm_i2clcd as lcd
 
 lcdMessage = " "
-myLCD = lcd.Jhd1313m1(6, 0x3E, 0x62)
+myLCD = lcd.Jhd1313m1(6, 0x3E, 0x62) # initialize LCD
 
 def loop():
   lcdMessage = "Hello, Hunter"
-  myLCD.setCursor(0,1)
-  myLCD.write(lcdMessage)
+  myLCD.setCursor(0,1) # set cursor
+  myLCD.write(lcdMessage) # write LCD message
   time.sleep(1)
   
 loop()
