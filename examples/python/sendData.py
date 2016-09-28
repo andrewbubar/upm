ee6a6fe13d2672b65b31fd080acd1eaca700d020
andrewbubar <andrew.bubar@yahoo.com>
2016-09-28 08:43:02 -0700

    Update sendData.py

--- examples/python/sendData.py
+++ examples/python/sendData.py
@@ -3,9 +3,12 @@
 import sqlite3 as lite
 import csv
 
+# write to backup.csv file
 with open('backup.csv', 'w+') as write_file:
-	con = lite.connect('makerspace.db')
-	cur = con.cursor()
+	con = lite.connect('makerspace.db') # connect to database
+	cur = con.cursor() # set cursor
+	
+	# retrieve values from table and write to file
 	for row in cur.execute('SELECT * FROM USED'):
 		write_file.write(','.join(row))
 		write_file.write('\n')
