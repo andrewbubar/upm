e748ad23e4b66fe4c24a018f29349fec866debaa
andrewbubar <andrew.bubar@yahoo.com>
2016-10-03 09:38:25 -0700

    Update newUser.py

--- examples/python/newUser.py
+++ examples/python/newUser.py
@@ -16,6 +16,7 @@ myLCD = lcd.Lcm1602(13,12,11,10,9,8)
 lcdMessage = " "
 
 con = lite.connect('makerspace.db')
+cur = con.cursor()
 
 blueLED.on()
 
@@ -30,6 +31,35 @@ def SIGINTHandler(signum, frame):
 
 def exitHandler():
 	print "Exiting"
+	myLCD.clear()
+	greenLED.off()
+	blueLED.off()
+	redLED.off()
+	con.close()
+	sys.exit(0)
+
+def registered():
+	'''
+	blueLED.off()
+	redLED.on()
+	myLCD.setCursor(0,0)
+	myLCD.write("RFID already")
+	myLCD.setCursor(1,0)
+	myLCD.write("registered")
+	time.sleep(1)
+	myLCD.clear()
+	blueLED.on()
+	'''
+	print ("RFID already registered")
+	
+def enterInfo():
+	name = raw_input('Enter the name: ')
+      	laser = raw_input('Laser Access? ')
+      	printer = raw_input('Printer Access? ')
+      	solder = raw_input('Solder Access? ')
+	params = (rfidNumber, name, laser, printer, solder)
+	cur.execute("INSERT INTO PERMISSIONS VALUES(?, ?, ?, ?, ?)", params)
+	con.commit()
 	sys.exit(0)
 	
 atexit.register(exitHandler)
@@ -46,51 +76,18 @@ myNFC.SAMConfig()
 uidSize = upmPn532.uint8Array(0)
 uid = upmPn532.uint8Array(7)
 
-cur = con.cursor()
-
 while (1):
 	for i in range(7):
 		uid.__setitem__(i, 0)
 	if (myNFC.readPassiveTargetID(upmPn532.PN532.BAUD_MIFARE_ISO14443A,
                                       uid, uidSize, 2000)):
 		# found a card
-		rfidData = []
-		for i in range(uidSize.__getitem__(0)):
-			rfidData.insert(i,uid.__getitem__(i))
-		rfidNumber = ''
-		for i in range(len(rfidData)):
-			rfidNumber = str(rfidNumber) + str(rfidData[i])
+		rfidNumber = getRFID()
 		cur.execute("SELECT * FROM PERMISSIONS where ID = ?", [rfidNumber])
 		result = cur.fetchone()
 		if result is not None:
-			#print("RFID is already registered")
-			blueLED.off()
-			redLED.on()
-			myLCD.setCursor(0,0)
-			myLCD.write("RFID already")
-			myLCD.setCursor(1,0)
-			myLCD.write("registered")
-			time.sleep(1)
-			myLCD.clear()
-			blueLED.on()
-			continue
+			registered()
 		else:
-      			name = raw_input('Enter the name: ')
-      			laser = raw_input('Laser Access? ')
-      			printer = raw_input('Printer Access? ')
-      			solder = raw_input('Solder Access? ')
-			params = (rfidNumber, name, laser, printer, solder)
-			cur.execute("INSERT INTO PERMISSIONS VALUES(?, ?, ?, ?, ?)", params)
-			con.commit()
-			con.close()
-			blueLED.off()
-			sys.exit(0)
+			enterInfo()
 	else:
-		#print "Waiting for a card...\n"
-		myLCD.setCursor(0,0)
-		lcdMessage = ("Waiting for")
-		myLCD.write(lcdMessage)
-		myLCD.setCursor(1,0)
-		lcdMessage2 = "a card . . ."
-		myLCD.write(lcdMessage2)
-		myLCD.clear()
+		waiting()
