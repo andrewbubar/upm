574a424d5b61c0e37a3524aec8ab72e8a5f1a87c
andrewbubar <andrew.bubar@yahoo.com>
2016-10-03 09:51:48 -0700

    Update updateUser.py

--- examples/python/updateUser.py
+++ examples/python/updateUser.py
@@ -12,8 +12,8 @@ greenLED = grove.GroveLed(GREEN_LED)
 blueLED = grove.GroveLed(BLUE_LED)
 redLED = grove.GroveLed(RED_LED)
 
-myLCD = lcd.Lcm1602(13,12,11,10,9,8)
-lcdMessage = " "
+#myLCD = lcd.Lcm1602(13,12,11,10,9,8)
+#lcdMessage = " "
 
 con = lite.connect('makerspace.db')
 cur = con.cursor()
@@ -31,7 +31,92 @@ def SIGINTHandler(signum, frame):
 
 def exitHandler():
 	print "Exiting"
+	#myLCD.clear()
+	greenLED.off()
+	blueLED.off()
+	redLED.off()
+	con.close()
 	sys.exit(0)
+	
+def waiting():
+	greenLED.off()
+	redLED.off()
+	print ("Waiting for a card")
+	'''
+  	lcdMessage = "Waiting for a"
+  	lcdMessage2 = "card . . ."
+  	myLCD.setCursor(0,0)
+	myLCD.write(lcdMessage)
+	myLCD.setCursor(1,0)
+	myLCD.write(lcdMessage2)
+	time.sleep(2)
+	myLCD.clear()
+	'''
+	
+def getRFID():
+	rfidData = []
+	for i in range(uidSize.__getitem__(0)):
+		rfidData.insert(i,uid.__getitem__(i))
+	rfidNumber = ''
+	for i in range(len(rfidData)):
+		rfidNumber = str(rfidNumber) + str(rfidData[i])
+	return rfidNumber
+
+def checkTable(number):
+	if len(number) > 0:
+		cur.execute("SELECT * FROM PERMISSIONS WHERE ID = ?", [number])
+		if TypeError:
+			return False
+		else:
+			return True
+		
+def fromTable(number):
+	if len(number) > 0:
+		cur.execute("SELECT * FROM PERMISSIONS WHERE ID = ?", [number])
+		result = cur.fetchone()
+		name = result[1]
+    		name = name.encode("utf-8")
+    		first, last = name.split()
+		laser = result[2]
+		printer = result[3]
+		solder = result[4]
+		#lcdMessage = "Hello " + first + "!"
+		#myLCD.setCursor(0,0)
+		#myLCD.write(lcdMessage)
+		#time.sleep(1)
+		#myLCD.clear()
+		return name, laser, printer, solder
+	
+def displayRFID(name, laser, printer, solder):
+	print ("This RFID is registered for: " + name)
+	print ("Laser Access: " + laser)
+	print ("3D Printer Access: " + printer)
+	print ("Solder Access: " + solder)
+	
+def updateRFID():
+	sel = raw_input("What would you like to update for this user? (Name, Laser, 3D Printer, or Solder)")
+	if sel == 'Name':
+		name = raw_input("What is the new name? ")
+		params = name, rfidNumber
+		cur.execute("UPDATE PERMISSIONS SET Name = ? WHERE ID = ?", params)
+	elif sel == 'Laser':
+		laser = raw_input("Y or N for Laser Cutter? ")
+		params = laser, rfidNumber
+		cur.execute("UPDATE PERMISSIONS SET Laser = ? WHERE ID = ?", params)
+	elif sel == '3D Printer':
+		printer = raw_input("Y or N for 3D Printer? ")
+		params = printer, rfidNumber
+		cur.execute("UPDATE PERMISSIONS SET Printer = ? WHERE ID = ?", params)
+	elif sel == 'Solder':
+		solder = raw_input("Y or N for Solder? ")
+		params = solder, rfidNumber
+		cur.execute("UPDATE PERMISSIONS SET Solder = ? WHERE ID = ?", params)
+	question = raw_input("Would you like to update another machine? (Yes or No)")
+	if question == 'Yes':
+		return True
+	else
+		return False
+
 	
 atexit.register(exitHandler)
 signal.signal(signal.SIGINT, SIGINTHandler)
@@ -57,62 +142,14 @@ while (1):
 		checkTable(rfidNumber)
 		if True:
 			name, laser, printer, solder = fromTable(rfidNumber)
-			print ("This RFID is registered for: " + name)
-			print ("Laser Access: " + laser)
-			print ("3D Printer Access: " + printer)
-			print ("Solder Access: " + solder)
-			question = 'Yes'
-			while question == 'Yes':
-				sel = raw_input("What would you like to update for this user? (Name, Laser, 3D Printer, or Solder)")
-				if sel == 'Name':
-					name = raw_input("What is the new name? ")
-					params = name, rfidNumber
-					cur.execute("UPDATE PERMISSIONS SET Name = ? WHERE ID = ?", params)
-				elif sel == 'Laser':
-					laser = raw_input("Y or N for Laser Cutter? ")
-					params = laser, rfidNumber
-					cur.execute("UPDATE PERMISSIONS SET Laser = ? WHERE ID = ?", params)
-				elif sel == '3D Printer':
-					printer = raw_input("Y or N for 3D Printer? ")
-					params = printer, rfidNumber
-					cur.execute("UPDATE PERMISSIONS SET Printer = ? WHERE ID = ?", params)
-				elif sel == 'Solder':
-					solder = raw_input("Y or N for Solder? ")
-					params = solder, rfidNumber
-					cur.execute("UPDATE PERMISSIONS SET Solder = ? WHERE ID = ?", params)
-				question = raw_input("Would you like to update another machine? (Yes or No)")
+			displayRFID(name, laser, printer, solder)
+			TF = True
+			while TF == True:
+				TF = updateRFID()
 			con.commit()
-			con.close()
-			blueLED.off()
 			sys.exit(0)
 		else:
 			print ("RFID is not registered")
-		result = cur.fetchone()
-		try:
-			print ("This RFID is registered for: " + result[1])
-			if result is not None:
-				sel = raw_input("What would you like to update for this user? ")
-				if sel == 'Name':
-			 		name = raw_input("What is the new name? ")
-		  	 		params = name, rfidNumber
-					cur.execute("UPDATE PERMISSIONS SET Name = ? WHERE ID = ?", params)
-				if sel == 'Laser':
-					laser = raw_input("Y or N for Laser Cutter? ")						params = laser, rfidNumber
-			  		cur.execute("UPDATE PERMISSIONS SET Laser = ? WHERE ID = ?", params)
-				if sel == 'Printer':
-		  	 		printer = raw_input("Y or N for 3-D Printer? ")
-					params = printer, rfidNumber
-		 			cur.execute("UPDATE PERMISSIONS SET Printer = ? WHERE ID = ?", params)
-				if sel == 'Solder':
-					solder = raw_input("Y or N for Soldering Machine? ")
-			 		params = solder, rfidNumber
-			 		cur.execute("UPDATE PERMISSIONS SET Solder = ? WHERE ID = ?", params)
-				con.commit()
-				con.close()
-				blueLED.off()
-				sys.exit(0)
-				
-		except:
-			print ("RFID is not registered")
+		
 	else:
 		waiting()
